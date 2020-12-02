9452599004d4e87180d57b9d4d741b48513c0f9c
Mauricio <hector.bravo@usantoto.edu.co>
2020-12-02 15:47:04 -0500

    proyecto

---
+++ Proyecto_final/Facturacion_ProyectoFinal/src/formulario.cleintes.java
@@ -0,0 +1,9 @@
+//AUTORES: Mauricio Bravo Y ESteban Gaona
+//DESCRIPCION: este es el proyecto final sobre un software de facturacion
+//DATE: 29-11-20
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class formulario_Clientes extends JFrame{
